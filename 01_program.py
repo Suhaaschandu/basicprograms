cbe0cd47c6a8c704b9625a6912466c57ad32520c
Suhaaschandu <suhaastadikonda1902@gmail.com>
2025-12-18 15:33:17 +0530

    Update 01_program.py

--- 01_program.py
+++ 01_program.py
@@ -1,13 +1,7 @@
-"""
 num1=int(input("Enter the first number:"))
 num2=int(input("Enter the second number:"))
 print("Addition: ",num1+num2)
 print("Subtraction: ",num1-num2)
 print("Multiplication: ",num1*num2)
 print("Division: ",num1/num2)
-"""
 
-first_name=input("Enter your first name:")
-last_name=input("Enter your last name:")
-full_name = first_name + ' ' +last_name
-print("Hello,",full_name,"! Welcome to python program.")
